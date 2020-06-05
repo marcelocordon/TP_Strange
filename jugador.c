de001fbe9628626b83c6093248722e9b59912c86
marcelocordon <66125157+marcelocordon@users.noreply.github.com>
2020-06-05 01:04:04 -0300

    Add files via upload

---
+++ jugador.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "demogorgon.h"
+
+const int DEF_ANIMO_JUGADOR = 0;
+const char DEF_NIVEL_CONFIANZA_JUGADOR = 'w';
+const float DEF_FUERZA_PSIQUICA = -1.0F;
+
+
+int main(){
+
+	
+	return 0;
+}
